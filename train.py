0ccfa6d3da1fd005c9ecdc4ef80a6fe9b629ed90
Seitoku Doi <j4338nhu@sit.ac.jp>
2025-12-09 15:48:50 +0900

    学習時間(1エポック)の計測

--- train.py
+++ train.py
@@ -66,8 +66,10 @@ train_acc_log = []
 val_acc_log = []
 for epoch in range(n_epochs):#n_epochs回繰り返す
     print(f'epoch {epoch+1}/{n_epochs}')
+    time_start = time.time()
     train_loss = models.train(model, dataloader_train, loss_fn, optimizer)
-    print(f'    training loss: {train_loss}')
+    time_end = time.time()
+    print(f'    training loss: {train_loss} ({time_end - time_start}s)')
     train_loss_log.append(train_loss)
 
     val_loss = models.test(model, dataloader_train, loss_fn)
